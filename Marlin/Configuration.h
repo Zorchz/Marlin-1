f47a002b9a5a540b6bad4453a0c828c5efdf809e
siarhei pantislaei <5029090@gmail.com>
2020-10-08 00:13:29 +0300

    correct NOZZLE_TO_PROBE_OFFSET for Z end stop

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -981,7 +981,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 37.197, -20.358, -2.90 }
+#define NOZZLE_TO_PROBE_OFFSET { 37.197, -20.358, -0.3 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
