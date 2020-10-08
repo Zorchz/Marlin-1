1de8c7230423b30da7a00ee71328341c0d36e33b
Siarhei Pantsialei <Zorch.z@gmail.com>
2020-10-08 10:50:56 +0300

    Increased current for Y and E

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2283,7 +2283,7 @@
   #endif
 
   #if AXIS_IS_TMC(Y)
-    #define Y_CURRENT       800
+    #define Y_CURRENT       900
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
@@ -2331,7 +2331,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      800
+    #define E0_CURRENT      900
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
