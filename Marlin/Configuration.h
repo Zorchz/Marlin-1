cb3f3aa5c5848c00310759c1164b420ba8a1afbb
Siarhei Pantsialei <Zorch.z@gmail.com>
2020-11-29 12:55:35 +0300

    fixed Z steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -736,7 +736,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160.85, 160.47, 499.8, 97.57 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160.85, 160.47, 399.8, 97.57 }
 
 /**
  * Default Max Feed Rate (mm/s)
