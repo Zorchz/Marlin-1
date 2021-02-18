faeae343a5fd2d73a86489a4d05c2a79753d4e98
Siarhei Pantsialei <Zorch.z@gmail.com>
2021-02-18 08:24:00 +0300

    Update pid nozze

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -509,9 +509,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp 13.77
-    #define DEFAULT_Ki 0.90
-    #define DEFAULT_Kd 52.73
+    #define DEFAULT_Kp 13.35
+    #define DEFAULT_Ki 0.85
+    #define DEFAULT_Kd 52.24
   #endif
 #endif // PIDTEMP
 
