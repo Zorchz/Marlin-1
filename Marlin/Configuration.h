4183fb0ba88580776166ed8cc16991caf99d347e
Zorch <Zorch.z@gmail.com>
2021-02-27 23:22:00 +0300

    fix merge issue

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1698,16 +1698,10 @@
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
-<<<<<<< HEAD
-#define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    110
-#define PREHEAT_2_TEMP_CHAMBER 35
-=======
 #define PREHEAT_2_LABEL       "PETG"
 #define PREHEAT_2_TEMP_HOTEND 230
 #define PREHEAT_2_TEMP_BED    70
->>>>>>> a26f3d1d23 (Updated Z offset)
+#define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 /**
